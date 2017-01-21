e3c5841e8a1a4c94db09800cb82891172ff60e2d
Hunjoo Lee <leehunz@naver.com>
2017-01-21 10:59:32 +0900

    update export to module.exports

--- www/brightness.js
+++ www/brightness.js
@@ -2,14 +2,14 @@
 
 var exec = require('cordova/exec');
 
-exports.getBrightness = function( success, error) {
-  exec(success, error, 'Brightness', 'getBrightness', []);
-};
-
-exports.setBrightness = function(value, success, error) {
-  exec(success, error, 'Brightness', 'setBrightness', [value]);
-};
-
-exports.setKeepScreenOn = function(value, success, error) {
-  exec(success, error, 'Brightness', 'setKeepScreenOn', [value]);
+module.exports = {
+  getBrightness: function( success, error) {
+    exec(success, error, 'Brightness', 'getBrightness', []);
+  },
+  setBrightness: function(value, success, error) {
+    exec(success, error, 'Brightness', 'setBrightness', [value]);
+  },
+  setKeepScreenOn: function(value, success, error) {
+    exec(success, error, 'Brightness', 'setKeepScreenOn', [value]);
+  }
 };
